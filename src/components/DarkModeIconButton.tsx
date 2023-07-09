1d151c0c48d04f700aa1d3f6a8c3eda77eecd4ec
luis-moguea <luis.mogueadp@gmail.com>
2023-07-09 15:42:20 -0500

    Add views images

--- src/components/DarkModeIconButton.tsx
+++ src/components/DarkModeIconButton.tsx
@@ -1,8 +1,7 @@
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { IconButton, useColorMode } from "@chakra-ui/react";
-import React from "react";
 
-const DarkModeIconButton: React.FC = () => {
+const DarkModeIconButton = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   const isDark = colorMode === "dark";
 
